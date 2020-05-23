85880c69ae5f8cb53f41611acf1fd9185adabf4b
KlugeKaindl <akk-gthb@team33.de>
2020-05-23 12:47:54 +0200

    basic project structure

---
+++ src/main/java/de/team33/apps/browser/Main.java
@@ -0,0 +1,12 @@
+package de.team33.apps.browser;
+
+public class Main implements Runnable {
+
+    public static void main(final String[] args) {
+    }
+
+    @Override
+    public final void run() {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+}
---
+++ src/test/java/de/team33/test/browser/MainTrial.java
@@ -0,0 +1,19 @@
+package de.team33.test.browser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTrial {
+
+    @SuppressWarnings("ConfusingMainMethod")
+    @Test
+    public final void main() {
+        fail("not yet implemented");
+    }
+
+    @Test
+    public final void run() {
+        fail("not yet implemented");
+    }
+}
